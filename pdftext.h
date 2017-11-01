76b46b387a406feb4ce71dcdf434f2b0b47854bb
sgerwk <sgerwk@aol.com>
2017-11-01 10:57:28 +0100

    spit text-printing functions to a separate file

---
+++ pdftext.h
@@ -0,0 +1,90 @@
+/*
+ * pdftext.h
+ *
+ * convert pdf to text
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <poppler.h>
+#include "pdfrects.h"
+
+/*
+ * parameters for the input (D=delta, %=percentage of page)
+ */
+struct measure {
+	int newline;		/* more Dy than this is a newline */
+	int newpar;		/* more Dy than this is a new paragraph */
+	int rightreturn;	/* line end before this x% is a new paragraph */
+	int newcolumnx;		/* more than this Dx% is new column (and) */
+	int newcolumny;		/* more than this -Dy% is new column (and) */
+	int indenttolerance;	/* tolerance for x at start of line */
+	int headfooter;		/* ignore x,y of chars at begin/end of page */
+	int textdistance;	/* distance between blocks of text */
+};
+
+/*
+ * output strings
+ */
+struct format {
+	char *parstart;		/* paragraph start */
+	char *parend;		/* paragraph end */
+
+	char *fontname;		/* format for printing font names, or NULL */
+
+	char *plain;		/* set font face */
+	char *italic;
+	char *bold;
+	char *bolditalic;
+
+	char *italicbegin;	/* begin/end font face */
+	char *italicend;
+	char *boldbegin;
+	char *boldend;
+
+	gboolean reset;		/* reset and restart face at par breaks */
+
+	char *backslash;
+	char *firstdot;		/* substitute this for dot at start of line */
+	char *less;
+	char *greater;
+	char *and;
+};
+
+/*
+ * known output formats
+ */
+extern struct format format_roff;
+extern struct format format_html;
+extern struct format format_tex;
+extern struct format format_textfont;
+extern struct format format_text;
+
+/* print reason for a paragraph break */
+extern gboolean debugpar;
+
+/* show the characters in a box */
+void showbox(FILE *fd, PopplerPage *page, PopplerRectangle *zone,
+		int method, struct measure *measure, struct format *format,
+		gboolean *newpar, char **prev);
+
+/* show the characters in a page */
+void showpage(FILE *fd, PopplerPage *page,
+		int method, struct measure *measure, struct format *format,
+		gboolean *newpar, char **prev);
+
+/* end a document */
+void enddocument(FILE *fd, struct format *format, char *prev);
+
+/* show a pdf document */
+void showdocument(FILE *fd, PopplerDocument *doc, int method,
+		struct measure *measure, struct format *format);
+
+/* show a pdf file */
+void showfile(FILE *fd, char *filename, int method,
+		struct measure *measure, struct format *format);
+
+/* parse a string into a struct format */
+struct format *parseformat(char *s);
+
