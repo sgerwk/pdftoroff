1ec1b3973b9e8cea69b62922a0889b1f9bc89590
sgerwk <sgerwk@aol.com>
2019-08-23 10:02:59 +0200

    remove function no longer existing

--- cairoio-x11.h
+++ cairoio-x11.h
@@ -11,11 +11,5 @@
  */
 struct cairodevice cairodevicex11;
 
-/*
- * parse the x11 cmdline option -x
- */
-int setinitdata(int argn, char *argv[], char *mainopts,
-		struct cairodevice *cairodevice);
-
 #endif
 
